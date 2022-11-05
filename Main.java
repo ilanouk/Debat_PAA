64118f5d88c86bba7fe5897b0bf2a939fb46ee71
MrGran1 <tigran.wattrelos@gmail.com>
2022-11-05 15:40:18 +0100

    test

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ public class Main {
         int i=0;
         while (i<nb){
             listeArgument.add(new Argument("A"+i)); // on crée les arguments et on les rentre dans la liste des arguments
+            i++;
         }
 
         while (boucle){
@@ -27,8 +28,8 @@ public class Main {
                 String strDepart = stContradiction.nextToken();
                 String strArrivee = stContradiction.nextToken();
                 
-                Argument argDepart;
-                Argument argArrivee;
+                Argument argDepart=null;
+                Argument argArrivee=null;
                 
                 for(i=0;i<listeArgument.size();i++){ //Dans cette boucle, on ajoute une contradiction entre les arguments donné par l'utilisateur
                     if (listeArgument.get(i).isEqual(strDepart)){
@@ -63,7 +64,7 @@ public class Main {
 
         boucle = true;
         while(boucle){
-            System.out.println("1) Ajouter un argument \n 2)retirer un argument \n Verifier la solution \n fin")
+            System.out.println("1) Ajouter un argument \n 2)retirer un argument \n Verifier la solution \n fin");
             int choix = sc.nextInt();
 
             if (choix ==1){ //ajouter un argument
