e51177fcf6034236486b9568c2b9c1820eca794a
TrTerry <117445844+TrTerry@users.noreply.github.com>
2022-11-05 17:43:10 +0100

    TEST

--- Main.java
+++ Main.java
@@ -65,11 +65,35 @@ public class Main {
         while(boucle){
             System.out.println("1) Ajouter un argument \n 2)retirer un argument \n 3) Verifier la solution \n 4) fin");
             int choix = sc.nextInt();
+            String argu = sc.nextLine();
 
             if (choix ==1){ //ajouter un argument
-
+            	System.out.println("Donnez l'argument que vous voulez ajouter :");
+            	argu = sc.next();
+            	for(int j = 0 ; j<listeArgument.size();j++) {
+            		if (listeArgument.get(j).isEqual(argu)) {
+            			System.out.println("Argument déjà ajouter");
+            		
+            	    } else {
+            		listeArgument.add(new Argument(argu));	
+            	    }
+            	}
             }
-            if (choix ==2){// supprimer un arrgument
+            System.out.println(listeArgument);
+            if (choix ==2){// supprimer un argument
+            	System.out.println("Donnez l'argument que vous voulez supprimer :");
+            	argu = sc.next();
+            	for(int k = 0 ; k<listeArgument.size();k++) {
+            		if (listeArgument.get(k).isEqual(argu)) {
+            			listeArgument.remove(k);
+            		
+            	    } else {
+            	    	System.out.println("Argument n'est pas dans la liste ou déjà supprimé");
+            	    }	
+                
+                }
+            System.out.println(listeArgument);
+            }
                 
             }
             if (choix ==3){// Verifier la solution
@@ -81,3 +105,4 @@ public class Main {
         }
     }
 }
+
