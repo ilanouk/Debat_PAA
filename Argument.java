9d1c5e7df7e85c27c39396e02ff68056655cd477
MrGran1 <tigran.wattrelos@gmail.com>
2022-11-05 18:01:04 +0100

    ilan

--- Argument.java
+++ Argument.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 
-import java.util.ArrayList;
 
 
 public class Argument{
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
-import java.util.Collection;
+
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main (String[]args){
